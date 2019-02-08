bf8b4072551c5ee484f67ecf5205f0ad6a5b38b7
Tars <taa522@Mail.umkc.edu>
2019-02-08 10:34:07 -0600

    test

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -1,3 +1,29 @@
+class Node:
+    def __init__(self, puzzle_matrix, level, f_score):
+        self.puzzle_matrix = puzzle_matrix
+        self.level = level
+        self.f_score = f_score
+
+    def find_empty(self, input_matrix, empty_char):
+        for i in range(0,len(self.puzzle_matrix)):
+            for j in range(0, len(self.puzzle_matrix)):
+                if input_matrix[i][j] == empty_char:
+                    return i,j
+
+    def generate_child(self):
+        x,y = self.find_empty(self.puzzle_matrix, 'E')
+        pos_values = [[x,y-1], [x,y+1], [x-1,y], [x+1,y]]
+        children = []
+
+        for i in pos_values:
+            child = self.shuffle(self.puzzle_matrix,x,y,i[0],i[1])
+            if child is not None:
+                child_node = Node(child, self.level+1,0)
+                children.append(child_node)
+        return children
+    #TODO: Write shuffle function
+
 class eightPuzzle:
-    def __init__(self, fileName):
-        puzzle_grid = open()
+    def __init__(self, inputMatrix):
+        self.puzzle_matrix = inputMatrix
+        self.empty_location = zip(*np.where)
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 'E']]
+
+goal_state = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', 'E']]
+
+with open('prog1_input.txt') as f:
+    num_puzzles = int(f.readline())
+    print(num_puzzles)
+    for i in range(num_puzzles):
+        current_puzzle = []
+
+        # eight puzzle means three lines of numbers
+        for j in range(3):
+            current_line = f.readline().strip('\n')
+            while current_line == '':
+                current_line = f.readline().strip('\n')
+            current_puzzle.append(current_line.split())
+
+        print(current_puzzle)
+        #for k in current_puzzle:
+            #print(k)
+        #print('\n')
+
 
